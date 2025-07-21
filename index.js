bd47b455531487fb55e5cf55da7061b45cdf70f2
Ayelen Rozas <ayelen29rozas@gmail.com>
2025-07-21 10:34:20 +0200

    1commit

---
+++ index.js
@@ -0,0 +1,699 @@
+const zapatillas = [
+  {
+id: 1,
+nombre: "Air Jordan 1 High OG",
+público: ["hombre","mujer"], 
+precio: 189.99,
+img: "./assets/menhigh1.png",
+talla:[38, 39, 40, 41, 42, 43, 44, 45],
+perfil: "alto",
+color: ["black","white","blue"], 
+  },
+
+  {
+id: 2,
+nombre: "Air Jordan 1 High OG",
+público: ["hombre","mujer"],
+precio: 189.99,
+img: "./assets/menhigh2-women.png",
+talla: [ 36, 37, 38, 39, 40,],
+perfil: "alto",
+color: ["beige","white","orange"],
+  },
+
+  {
+id: 3,
+nombre: "Air Jordan I High G",
+público: ["hombre","mujer"],
+precio: 199.99,
+img: "./assets/menhigh3-women.png",
+talla: [ 36, 37],
+perfil: "alto",
+color: ["black","white","gold"],
+  },
+
+  {
+id: 4,
+nombre: "Air Jordan 1 Mid",
+público: "hombre",
+precio: 139.99,
+img: "./assets/menmid1.png",
+talla:[38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48],
+perfil: "medio",
+color: ["blue","white","beige"],
+  },
+
+  {
+id: 5,
+nombre: "Jumpman MVP",
+público: "hombre",
+precio: 169.99,
+img: "./assets/menmid2.png",
+talla: [ 40, 41, 42, 43, 45, 46],
+perfil: "medio",
+color: ["white","red"],
+  },
+
+  {
+id: 6,
+nombre: "Air Jordan 1 Mid SE",
+público: "hombre",
+precio: 149.99,
+img: "./assets/menmid3.png",
+talla:[ 40, 41, 42, 43, 44, 45,48, 50, 51],
+perfil: "medio",
+color: ["brown","white","grey"],
+  },
+
+  {
+id: 7,
+nombre: "Air Jordan 1 Low SE",
+público: "hombre",
+precio: 139.99,
+img: "./assets/menLow1.png",
+talla:[38, 39, 40, 41, 42, 43, 44, 45],
+perfil: "bajo",
+color: ["black","white","grey"],
+  },
+
+  {
+id: 8,
+nombre: "Air Jordan 1 Low SE",
+público: "hombre",
+precio: 139.99,
+img: "./assets/menLow2.png",
+talla:[40, 41, 42, 43, 44, 45, 46, 47],
+perfil: "bajo",
+color: ["black","white","grey","beige"],
+  },
+
+  { id: 9,
+nombre: "Air Jordan 1 Low Premium",
+público: "hombre",
+precio: 139.99,
+img: "./assets/menLow3.png",
+talla:[38, 39, 40, 41, 42, 43, 44, 45],
+perfil: "alto",
+color: ["beige","brown"],
+  },
+
+  {
+id: 10,
+nombre: "Air Jordan 1 Retro High Ruby ",
+público: "mujer",
+precio: 189.99,
+img: "./assets/womenhigh1.png",
+talla:[35, 36, 37,38, 39, 40, 41, 42, 43, 44, 45],
+perfil: "alto",
+color: ["black","white","red"],
+  },
+
+  {
+id: 11,
+nombre: "Air Jordan 1 Mid SE",
+público: "mujer",
+precio: 149.99,
+img: "./assets/womenmid1.png",
+talla: [35,36,37, 38, 39, 40, 41, 42, 43, 44, 45],
+perfil: "medio",
+color: ["black","white","orange"],
+  },
+
+  {
+id: 12,
+nombre: "Air Jordan 1 Mid ",
+público: "mujer",
+precio: 139.99,
+img: "./assets/womenmid2.png",
+talla: [35,36,37,38, 39, 40, 41, 42, 43, 44, 45],
+perfil: "medio",
+color: ["black","white"],
+  },
+  {
+id: 13,
+nombre: "Air Jordan 1 Mid ",
+público: "mujer",
+precio: 139.99,
+img: "./assets/womenmid3.png",
+talla: [35,36,37,38, 39, 40, 41, 42, 43],
+perfil: "medio",
+color: ["brown","white","grey"],
+  },
+  {
+id: 14,
+nombre: "Air Jordan 1 Brooklyn Low",
+público: "mujer",
+precio: 159.99,
+img: "./assets/womenlow1.png",
+talla: [35,36,37,38, 39, 40, 41, 42],
+perfil: "bajo",
+color: ["silver","white"],
+  },
+
+  {
+id: 15,
+nombre: "Air Jordan 1 Low ",
+público: "mujer",
+precio: 129.99,
+img: "./assets/womenlow2.png",
+talla: [35,36,37,38, 39, 40, 41, 42, 43, 44],
+perfil: "bajo",
+color: ["orange","white"],
+  },
+
+  {
+id: 16,
+nombre: "Air Jordan 1 Low SE",
+público: "mujer",
+precio: 139.99,
+img: "./assets/womenlow3.png",
+talla: [35,36,37,38, 39, 40, 41, 42, 43, 44, 45],
+perfil: "medio",
+color: ["black","white","purple"],
+  },
+
+  {
+id: 17,
+nombre: "Nike Dunk Low",
+público: "niño",
+precio: 69.99,
+img: "./assets/kids1.png",
+talla: [27, 28, 29, 30, 31, 32, 33, 34, 35],
+perfil: "bajo",
+color: ["black","white"],
+  },
+  {
+id: 18,
+nombre: "Jordan 1 Low Alt",
+público: "niño",
+precio: 74.99,
+img: "./assets/kids2.png",
+talla: [27, 28, 29, 30, 31, 32, 33, 34, 35],
+perfil: "bajo",
+color: ["blue","white"],
+  },
+  {
+id: 19,
+nombre: " Jordan 1 Low Alt ",
+público: "niño",
+precio: 74.99,
+img: "./assets/kids3.png",
+talla: [27, 28, 29, 30, 31, 32, 33, 34, 35],
+perfil: "bajo",
+color: ["pink","white"],
+  },
+  {
+id: 20,
+nombre: " Air Jordan 3 Rare Air",
+público: "bebe",
+precio: 209.99,
+img: "./assets/baby1.png",
+talla:[ 36, 37],
+perfil: "bajo",
+color:["black","white","blue"],
+  },
+  {
+id: 21,
+nombre: " Jordan 1 Low Alt",
+público: "bebe",
+precio: 38.99,
+img: "./assets/baby2.png",
+talla: [17, 18, 19, 20, 21, 22, 24, 25, 27],
+perfil: "alto",
+color: ["grey","white"],
+  },
+  {
+id: 22,
+nombre: " Jordan 1 Mid SE",
+público: "bebe",
+precio: 69.99,
+img: "./assets/baby3.png",
+talla: [17, 18, 19, 20, 21, 22, 24, 25, 27],
+perfil: "alto",
+color: ["blue","white","pink","grey"],
+  },
+
+];
+//para crear nav con barra menu desplegable en responsive
+//1 tenemos array del anvegador con contenido que tendra la lista
+const barraNavs=[
+  {text:'Novedades y Destacados',url: 'https://www.nike.com/es/w/nuevo-3n82y' },
+  {text:'Zapatillas',url:'#Zapatillas'},
+  {text:'Carrito',url: 'https://www.nike.com/es/cart' }];
+//2 selecciono header sobre el que voy a trabajar
+const head=document.querySelector('header');
+//3 creo nav donde voy a meter ul y li con array,length
+const nav=document.createElement('nav');
+//4 le doy clase flex 
+nav.className='flexContainer';
+const ul=document.createElement('ul');
+for (let i=0; i<barraNavs.length; i++){
+    const barraNav = barraNavs[i];
+    const li = document.createElement('li');
+    const ancor=document.createElement('a');
+    ancor.href = barraNav.url; 
+    ancor.textContent = barraNav.text; 
+    li.appendChild(ancor);
+		ul.appendChild(li);
+}
+//imagen para utilizar con mediaq para el responsive
+const imgNav=document.createElement('img');
+imgNav.src='./assets/siluetaBlanca.png';
+imgNav.className='siluetaB';
+
+nav.appendChild(imgNav);
+nav.appendChild(ul);
+head.appendChild(nav);
+//evento para cambiar de clase la imagen,al hacer click despliega el menu uno abajo de otro
+document.querySelector('img').addEventListener("click", () => document.querySelector("ul").classList.toggle("closed"));
+document.body.appendChild(head);
+
+//hacer un main y dentro un section donde pondremos cada div con su zapatilla y su informacion
+const myMain= document.createElement('main');
+const mySection= document.createElement('section');
+mySection.className='flexContainer';
+mySection.id='Zapatillas';
+
+//para crear los filtros voy a crear las const de cada uno
+const Perfiles=[];
+const Publicos=[];
+const Colores=[];
+let Perfil='';
+let Talla='';
+const tallasUnicas=[];
+let color='';
+let público='';
+
+/*voy a declarar todos los divs que contienen los filtros fuera para luego llevarlos a un mismo section*/
+const divFiltro1=document.createElement('div');
+const divFiltro2= document.createElement('div');
+const divFiltro3= document.createElement('div');
+const divFiltro4=document.createElement('div');
+
+/*FILTRO POR PERFIL*/ 
+const filtrar= ()=>{
+const filtered=[];
+for (const zapatilla of zapatillas) {
+  if(zapatilla.perfil===Perfil){
+    filtered.push(zapatilla);
+  }
+}
+impZapas(filtered);
+}
+const fillPerfil=()=>{
+  //filtramos para eliminar los repetidos y solo subir al array elementos unicos
+  Perfiles.splice(0);
+  for (const zapatilla of zapatillas) {
+    if(!Perfiles.includes(zapatilla.perfil)){
+      Perfiles.push(zapatilla.perfil);
+    }
+  }
+}
+fillPerfil();
+
+//funcion para iterar en cada zapatilla el valor del perfil y ponerlo en la option
+const createSelectPerfiles=()=>{
+  const SelectPerfiles=document.createElement('select');
+  for(const perfil of Perfiles) {
+    const option=document.createElement('option');
+    option.value=perfil;
+    option.textContent=perfil;
+    SelectPerfiles.appendChild(option);
+  }
+  divFiltro1.appendChild(SelectPerfiles);
+  SelectPerfiles.addEventListener("change",(e)=> {
+    Perfil=e.target.value;
+    filtrar();
+  });
+} // como los pone a todos hay que crear un array donde no los repita
+
+/*FILTRO POR TALLAS*/ 
+const fillTallas=()=>{
+  //filtro para eliminar los repetidos y solo subir al array elementos unicos
+tallasUnicas.splice(0);//limpiar el array
+  for (const zapatilla of zapatillas) {
+    if (Array.isArray(zapatilla.talla)) {//usamos array.isaarray para eliminar los que no fueran array o numero
+      for (const t of zapatilla.talla) {
+          if (!tallasUnicas.includes(t)) {
+          tallasUnicas.push(t);
+            }
+  }
+}
+}
+tallasUnicas.sort((a, b) => a - b); // ordeena por numero las tallas
+}
+fillTallas();
+//////
+const filtrarT= ()=>{
+  const filtered = zapatillas.filter(z =>
+    Array.isArray(z.talla) && z.talla.includes(parseInt(Talla))
+  );
+  impZapas(filtered);
+}
+const createSelectTallas= ()=>{
+  const SelectTallas=document.createElement('select');
+  for (const t of tallasUnicas) {
+    const option2 = document.createElement('option');
+    option2.value = t;
+    option2.textContent = t;
+    SelectTallas.appendChild(option2);
+  }
+  divFiltro2.appendChild(SelectTallas);
+  SelectTallas.addEventListener("change",(e)=>
+  { Talla= e.target.value;
+    filtrarT()
+  })
+}
+/*FILTRO POR COLOR*/
+const fillColores=()=>{
+  Colores.splice(0);
+    for (const zapatilla of zapatillas) {
+    const zcolor =Array.isArray(zapatilla.color)
+    ? zapatilla.color
+    :[zapatilla.color]
+    for (const zc of zcolor) {
+      if (!Colores.includes(zc)){
+      Colores.push(zc);
+    }
+  }}
+
+/*const ColorDoble= ()=>{
+for(let i=0; i<Colores.length; i++){
+const priColor=Colores[i];
+for (let j=i+1; j<Colores.length;j++){
+const segColor=Colores[j];
+if(priColor===segColor){
+Colores.splice(j,1);
+j--;}
+} modifico la funcion en dos for of, para reducir codigo y simplificar con los ? : !
+}simplificado puedo copiar en la siguiente funcion 
+}
+ColorDoble();*/
+};
+fillColores();
+//////
+  const filtrarC= ()=>{
+  const filtered = zapatillas.filter(z => 
+    //Array.isArray se usa para comprobar dentro del array el recorrido
+    Array.isArray(z.color) && z.color.includes(color)
+  );
+  impZapas(filtered);
+  }
+
+const createSelectColor= ()=>{
+  const SelectColor=document.createElement('select');
+  for (const color of Colores) {
+    const option3 = document.createElement('option');
+    option3.value = color;
+    option3.textContent = color;
+    SelectColor.appendChild(option3);
+  }
+  divFiltro3.appendChild(SelectColor);
+  SelectColor.addEventListener("change",(e)=>{
+  color= e.target.value;
+  filtrarC();
+  })
+};
+/*FILTRO PUBLICO*/
+
+
+const fillPublicos=()=>{
+  Publicos.splice(0);
+    for (const zapatilla of zapatillas) {
+      const pub=Array.isArray(zapatilla.público)
+    ? zapatilla.público
+    : [zapatilla.público];
+      //solo con array.isarray se suben los publicos que tienen array pero no los de string
+    for (const p of pub) {
+      if (!Publicos.includes(p)){
+      Publicos.push(p);
+      }
+    }
+}}
+fillPublicos();
+  const filtrarP= ()=>{
+  const filtered = zapatillas.filter(z => {
+    //Array.isArray se usa para comprobar dentro del array el recorrido
+    const pub=z.público;
+    if(Array.isArray(pub)) {
+      return pub.includes(público);
+    }else{
+      return pub===público;
+    }
+  });
+  impZapas(filtered);
+}
+
+//funcion para iterar en cada zapatilla el valor del perfil y ponerlo en la option
+const createSelectPublicos=()=>{
+  const SelectPublicos=document.createElement('select');
+  for(const público of Publicos) {
+    const option4=document.createElement('option');
+    option4.value=público;
+    option4.textContent=público;
+    SelectPublicos.appendChild(option4);
+  }
+  divFiltro4.appendChild(SelectPublicos);
+  SelectPublicos.addEventListener("change",(e)=> {
+    público=e.target.value;
+    filtrarP();
+  });
+}  
+//meter todos los divs a un section
+const todosFiltros= document.createElement('section');
+todosFiltros.className='flexContainer filtros';
+todosFiltros.appendChild(divFiltro1);
+todosFiltros.appendChild(divFiltro2);
+todosFiltros.appendChild(divFiltro3);
+todosFiltros.appendChild(divFiltro4);
+myMain.appendChild(todosFiltros);
+
+//funcion para unir los filtros
+
+function fillZapas(zapatillas){
+  const filteredZapas= []; //array vacio para subir las opciones de filtro elegidas
+  for (const zapatilla of zapatillas) {
+    let filteredOk=true;
+    //se deja en positivo pero se tiene que negar los otros if para verificar si cumple o no
+    if(Perfil&&zapatilla.perfil!==Perfil){//modifico y agrego!
+      filteredOk=false;
+    }
+    if(Publicos&&zapatilla.público!==Publicos){
+      filteredOk=false;
+    }
+    if(Colores&&zapatilla.color!==Colores){
+      filteredOk=false;
+    }
+    if(tallasUnicas&&zapatilla.talla!==tallasUnicas){
+      filteredOk=false;
+    }
+    if(filteredOk){
+      filteredZapas.push(zapatilla);
+    }}
+    console.log("Resultado el filtro",filteredZapas);
+    //agregar console.log de cada variable en if para ver que es lo que muestra
+    if (filteredZapas.length === 0) {
+    console.log('No se han encontrado resultados');//comprobar en consola antes los resultados
+    // se comenta porque no funciona correctamente//document.body.innerHTML+='<p>No se han encontrado resultados</p>';
+  }
+  return filteredZapas;
+}
+fillZapas(zapatillas);
+
+
+// voy a crear un boton para reiniciar todos los filtros
+
+const divButton= document.createElement('div');
+divButton.className='divbutton';
+//saco el contenedor del boton para luego agregarlo al section
+const cerofiltros=()=>{
+  //hago contenedor del button
+//button
+  const buttonFill= document.createElement('button');
+  buttonFill.className='buttonFill';
+  buttonFill.textContent='↻ Reiniciar Filtros';
+  divButton.appendChild(buttonFill);
+  buttonFill.addEventListener("click",reinicioB);
+
+  function reinicioB(){
+    //llamo a cada array para vaciarlo y luego a la funcion
+    Perfiles.splice(0);
+    fillPerfil();
+    tallasUnicas.splice(0);
+    fillTallas();
+    Colores.splice(0);
+    fillColores();
+    Publicos.splice(0);
+    fillPublicos();
+    Perfil = '';
+    Talla = '';
+    color = '';
+    público = '';
+
+    impZapas(zapatillas);
+  }
+
+}
+cerofiltros();
+todosFiltros.appendChild(divButton);
+
+//en esta funcion para cada zapatilla voy a generar un div que contiene la info, un div para la imagen con su imagen, h3 para nombre y ha4 para precio por ultimo boton
+const impZapas=(zapatillas)=>{
+  mySection.innerHTML = ''; 
+  //limpiar el section con cambios de filtros en zapatillas
+  for (let i=0; i<zapatillas.length;i++) {
+    //creo todos los elementos
+  const divz=document.createElement('div');
+  const divimgz=document.createElement('div');
+  const imgz= document.createElement('img');
+  const nombre= document.createElement('h3');
+  const precio= document.createElement('h4');
+  const buttonz= document.createElement('button');
+
+//le doy el contenido al elemento
+  divimgz.className='imgz';
+  imgz.src= zapatillas[i].img;
+  nombre.textContent=zapatillas[i].nombre;
+  precio.textContent= `$${zapatillas[i].precio}`;
+  buttonz.textContent=`Comprar`;
+//ubico dentro de cada uno
+  divz.appendChild(divimgz);
+  divimgz.appendChild(imgz);
+  divz.appendChild(nombre);
+  divz.appendChild(precio);
+  divz.appendChild(buttonz);
+  mySection.appendChild(divz);
+
+}
+//agrego el section a main y este al
+myMain.appendChild(mySection);
+document.body.appendChild(myMain);
+
+
+}
+fillPerfil();
+createSelectPerfiles();
+
+fillTallas();
+createSelectTallas();
+
+fillColores();
+createSelectColor();
+
+fillPublicos();
+createSelectPublicos();
+
+impZapas(zapatillas);
+//para hacer el footer lo selecciono porque ya esta hecho en html y paso los arrays de las 3 listas que tendran la info
+const footer=document.querySelector('footer');
+const Recursos= [
+{text: 'Tarjeta de regalo',
+url: 'https://www.nike.com/es/tarjetas-de-regalo'},
+{text: 'Tarjeta de regalo corporativa',
+url: 'https://nikegiftcardsforbusiness.com/'},
+{text: 'Buscar una tienda',
+url: 'https://www.nike.com/es/retail/'},
+{text: 'Nike journal',
+url: 'https://www.nike.com/es/historias'},
+{text:'Hazte Member',
+url: 'https://www.nike.com/es/inscripcion'},
+{text: 'Descuento para estudiantes',
+url: 'https://www.nike.com/es/student-discount'},
+{text: 'Comentarios',
+url: 'https://www.nike.com/es/#site-feedback'},
+{text: 'Códigos promocionales',
+url:'https://www.nike.com/es/codigo-promocional'},
+];
+const Ayuda= [
+{text: 'Obtener ayuda',
+url: 'https://www.nike.com/es/help'},
+{text: 'Estado del pedido',
+url: 'https://www.nike.com/es/orders/details'},
+{text: 'Envíos y entregas',
+url: 'https://www.nike.com/es/help/a/envio-entrega'},
+{text: 'Devoluciones',
+url: 'https://www.nike.com/es/help/a/politica-de-devolucion'},
+{text: 'Opciones de pago',
+url: 'https://www.nike.com/es/help/a/opciones-de-pago'},
+{text: 'Contacto',
+url: 'https://www.nike.com/es/help/#contact'},
+{text: 'Evaluaciones',
+url: 'https://www.nike.com/es/help/a/resenas'},
+{text: 'Ayuda con los códigos promocionales de Nike',
+url:'https://www.nike.com/es/help/a/aplicar-promo'},
+];
+const Empresa= [
+{text: 'Acerca de Nike',
+url: 'https://about.nike.com/en/?_gl=1*rbbkka*_gcl_aw*R0NMLjE3NTIzNjQ1MTQuQ2owS0NRandqOGpEQmhEMUFSSXNBQ1JWMlRzTWxLU3dkX1had3V5WU9oeTZVSFRBTUxFcDdQWnNlTm5NZGxOTVRKM3RoQVZ3bFA3N2pPVWFBbXRqRUFMd193Y0I.*_gcl_dc*R0NMLjE3NTIzNjQ1MTQuQ2owS0NRandqOGpEQmhEMUFSSXNBQ1JWMlRzTWxLU3dkX1had3V5WU9oeTZVSFRBTUxFcDdQWnNlTm5NZGxOTVRKM3RoQVZ3bFA3N2pPVWFBbXRqRUFMd193Y0I.*_gcl_au*MTM1MTIyMTI5OS4xNzUyMzY0MzUy*FPAU*MTM1MTIyMTI5OS4xNzUyMzY0MzUy'},
+{text: 'Novedades',
+url:'https://about.nike.com/en/newsroom' },
+{text: 'Empleo',
+url:'https://careers.nike.com/es' },
+{text: 'Inversores',
+url:'https://investors.nike.com/Home/default.aspx' },
+{text: 'Sostenibilidad',
+url:'https://www.nike.com/es/sostenibilidad' },
+{text: 'Accesibilidad',
+url:'https://www.nike.com/es/accessibility/statement'},
+{text: 'Propósito',
+url:'https://www.nike.com/es/proposito' },
+{text: 'Informar de un problema',
+url:'https://secure.ethicspoint.com/domain/media/eseu/gui/56821/index.html'},
+];
+const ulFooter1= document.createElement('ul');
+const divFo1=document.createElement('div');
+const h3FR=document.createElement('h3');
+h3FR.textContent='Recursos';
+for (let i=0; i<Recursos.length; i++) {
+  const recurso = Recursos[i];
+  const liF1=document.createElement('li');
+  const aF1=document.createElement('a');
+  liF1.text=aF1;
+  aF1.href=recurso.url;
+  aF1.textContent=recurso.text;
+  liF1.appendChild(aF1);
+  ulFooter1.appendChild(liF1);  
+}
+divFo1.appendChild(h3FR);
+divFo1.appendChild(ulFooter1);
+footer.appendChild(divFo1);
+
+
+const ulFooter2= document.createElement('ul');
+const divFo2=document.createElement('div');
+const h3Fa=document.createElement('h3');
+h3Fa.textContent='Ayuda';
+for (let i=0; i<Ayuda.length; i++) {
+  const ayud = Ayuda[i];
+  const liF2=document.createElement('li');
+  const aF2=document.createElement('a');
+  liF2.text=aF2;
+  aF2.href=ayud.url;
+  aF2.textContent=ayud.text;
+  liF2.appendChild(aF2);
+  ulFooter2.appendChild(liF2);  
+}
+divFo2.appendChild(h3Fa);
+divFo2.appendChild(ulFooter2);
+footer.appendChild(divFo2);
+
+
+const ulFooter3= document.createElement('ul');
+const divFo3=document.createElement('div');
+const h3Fe=document.createElement('h3');
+h3Fe.textContent='Empresa';
+for (let i=0; i<Empresa.length; i++) {
+  const emp = Empresa[i];
+  const liF3=document.createElement('li');
+  const aF3=document.createElement('a');
+  liF3.text=aF3;
+  aF3.href=emp.url;
+  aF3.textContent=emp.text;
+  liF3.appendChild(aF3);
+  ulFooter3.appendChild(liF3);  
+}
+divFo3.appendChild(h3Fe);
+divFo3.appendChild(ulFooter3);
+footer.appendChild(divFo3);
+document.body.appendChild(footer);
+
