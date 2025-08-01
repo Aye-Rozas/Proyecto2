4cdda4e1e608d0d84b153e00ee44d0b7bc3930c2
Ayelen Rozas <ayelen29rozas@gmail.com>
2025-08-01 15:00:29 +0200

    se descarta funcion general

--- src/components/funcGener.js
+++ src/components/funcGener.js
@@ -1,24 +1,20 @@
-/*import { zapatillas } from "./arrayZapatillas.js";
-console.log(zapatillas);*/
-
+import  {zapatillas}  from "./arrayZapatillas.js";
+//console.log(zapatillas);
+//
 /*const arrayGenerico=(valorGral, esNumero=false)=>{
   const valorU= new Set();
-  for (const z of zapatillas) {
-    const valores=Array.isArray(z[valorGral]) ? z[valorGral] : [z[valorGral]];
+  for (const zapa of zapatillas) {
+    const valores=Array.isArray(zapa[valorGral]) ? zapa[valorGral] : [zapa[valorGral]];
     for(const v of valores){ //error porque no habia const
       valorU.add(v)
     }
 }
-const resultado=[...valorU];
-console.log(`Valores únicos antes de ordenar (${valorGral}):`, resultado);
-//retorno o esta vacio, probamos con spread
-if (esNumero){
-  return resultado.map(Number).sort((a,b)=>a-b);
-}
-else{
-  return resultado.sort((a, b) => a.toString().toLowerCase().localeCompare(b.toString().toLowerCase()));
-}
+    const resultado = [...valorU];
+  return esNumero
+    ? resultado.map(Number).sort((a, b) => a - b)
+    : resultado.sort();
 };
+
 //const tallasUnicas=arrayGenerico("talla",true);
 //console.log(tallasUnicas);
 /*const Colores=arrayGenerico("color");
